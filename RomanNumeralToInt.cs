0aadbc4f5fc236d95ca9a4b803cac394a838b58c
John Deer <jtd6658@rit.edu>
2011-05-21 16:22:53 -0400

    One passing test.

---
+++ RomanNumeralToInt.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RomanNumeralToInt
+{
+    class RomanNumeralToInt
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public int convert(string num)
+        {
+            return 1;
+        }
+    }
+}
---
+++ RomanNumeralToIntTest.cs
@@ -0,0 +1,97 @@
+﻿using RomanNumeralToInt;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace RomanNumeralToIntTest
+{
+    
+    
+    /// <summary>
+    ///This is a test class for RomanNumeralToIntTest and is intended
+    ///to contain all RomanNumeralToIntTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class RomanNumeralToIntTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        private RomanNumeralToInt.RomanNumeralToInt instance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        [TestInitialize()]
+        public void MyTestInitialize()
+        {
+            instance = new RomanNumeralToInt.RomanNumeralToInt();
+        }
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for RomanNumeralToInt Constructor
+        ///</summary>
+        [TestMethod()]
+        public void RomanNumeralToIntConstructorTest()
+        {
+            Assert.IsNotNull(instance);
+        }
+
+        /// <summary>
+        ///A test for Main
+        ///</summary>
+        [TestMethod()]
+        [DeploymentItem("RomanNumeralToInt.exe")]
+        public void MainTest()
+        {
+            string[] args = null; 
+            RomanNumeralToInt_Accessor.Main(args);
+            Assert.IsNotNull(instance);
+        }
+
+        [TestMethod()]
+        public void testI()
+        {
+            Assert.Equals(1, instance.convert("I"));
+        }
+    }
+}
